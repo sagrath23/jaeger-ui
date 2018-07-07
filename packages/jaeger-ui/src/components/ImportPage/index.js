d94c2525f75f8a20e78f9b673bf3e16733802095
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2018-07-07 12:21:12 -0500

    add drag & drop antd upload component and fix layout of ImportPage component

--- packages/jaeger-ui/src/components/ImportPage/index.js
+++ packages/jaeger-ui/src/components/ImportPage/index.js
@@ -13,14 +13,39 @@
 // limitations under the License.
 
 import React from 'react';
-import { Col, Row } from 'antd';
+import { Col, Row, Form, Upload, Icon, message } from 'antd';
+
+const Dragger = Upload.Dragger;
+
+const props = {
+  name: 'file',
+  multiple: true,
+  action: '//jsonplaceholder.typicode.com/posts/',
+  onChange(info) {
+    const status = info.file.status;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
 
 const ImportTracePage = () => (
-  <Row>
+  <Row type="flex" align="center">
     <Col span={18}>
-      <form>
-        <input id="trace-file" type="file" />
-      </form>
+      <Form>
+        <Dragger {...props}>
+          <p className="ant-upload-drag-icon">
+            <Icon type="inbox" />
+          </p>
+          <p className="ant-upload-text">Click or drag file to this area to upload</p>
+          <p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
+        </Dragger>
+      </Form>
     </Col>
   </Row>
 );
